6f711777062dca7de6daca9885539f1b47c79af3
Gilberto <gilbertosvlucas@gmail.com>
2025-06-13 10:40:43 -0300

    criação botão excluir e alterar contas a pagar e receber

--- financeiro/urls.py
+++ financeiro/urls.py
@@ -6,5 +6,13 @@ urlpatterns = [
     path('receber/nova/', views.nova_conta_receber, name='nova_conta_receber'),
     path('pagar/', views.lista_contas_pagar, name='lista_contas_pagar'),
     path('pagar/nova/', views.nova_conta_pagar, name='nova_conta_pagar'),
+    
+    # Contas a Receber
+    path('receber/editar/<int:conta_id>/', views.editar_conta_receber, name='editar_conta_receber'),
+    path('receber/excluir/<int:conta_id>/', views.excluir_conta_receber, name='excluir_conta_receber'),
+
+    # Contas a Pagar
+    path('pagar/editar/<int:conta_id>/', views.editar_conta_pagar, name='editar_conta_pagar'),
+    path('pagar/excluir/<int:conta_id>/', views.excluir_conta_pagar, name='excluir_conta_pagar'),
 
 ]
--- financeiro/views.py
+++ financeiro/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import ContaReceber
 from .forms import ContaReceberForm
@@ -36,3 +37,44 @@ def nova_conta_pagar(request):
     else:
         form = ContaPagarForm()
     return render(request, 'financeiro/form_pagar.html', {'form': form})
+
+@login_required
+def editar_conta_receber(request, conta_id):
+    conta = get_object_or_404(ContaReceber, id=conta_id)
+    if request.method == 'POST':
+        form = ContaReceberForm(request.POST, instance=conta)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_contas_receber')
+    else:
+        form = ContaReceberForm(instance=conta)
+    return render(request, 'financeiro/form_receber.html', {'form': form})
+
+@login_required
+def excluir_conta_receber(request, conta_id):
+    conta = get_object_or_404(ContaReceber, id=conta_id)
+    if request.method == 'POST':
+        conta.delete()
+        return redirect('lista_contas_receber')
+    return render(request, 'financeiro/confirma_exclusao.html', {'conta': conta})
+
+@login_required
+def editar_conta_pagar(request, conta_id):
+    conta = get_object_or_404(ContaPagar, id=conta_id)
+    if request.method == 'POST':
+        form = ContaPagarForm(request.POST, instance=conta)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_contas_pagar')
+    else:
+        form = ContaPagarForm(instance=conta)
+    return render(request, 'financeiro/form_pagar.html', {'form': form})
+
+@login_required
+def excluir_conta_pagar(request, conta_id):
+    conta = get_object_or_404(ContaPagar, id=conta_id)
+    if request.method == 'POST':
+        conta.delete()
+        return redirect('lista_contas_pagar')
+    return render(request, 'financeiro/confirma_exclusao.html', {'conta': conta})
+
